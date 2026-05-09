982ec16846e992512c27ea985d713a706bd8911a
f47zhu <f47zhu@uwaterloo.ca>
2026-05-09 18:10:51 -0400

    nits: Export cards, misc nits

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,7 @@
 import Image from "next/image";
+import { PageCard, InnerCard } from "./cards"
 
 export default function Home() {
-
-  function PageCard({ id = "", className = "", color, title, children }:
-      { id?: string, className?: string, color: string, title: string, children: React.ReactNode }) {
-    const colorVariants: Record<string, string> = {
-      bordergreen: "border-green-700 dark:border-green-300",
-      textgreen: "text-green-800 dark:text-green-200",
-      borderred: "border-red-700 dark:border-red-300",
-      textred: "text-red-800 dark:text-red-200",
-      borderyellow: "border-yellow-700 dark:border-yellow-300",
-      textyellow: "text-yellow-800 dark:text-yellow-200",
-      borderorange: "border-orange-700 dark:border-orange-300",
-      textorange: "text-orange-800 dark:text-orange-200"
-    };
-    return (
-      <div id={id} className={`border-4 p-6 ${colorVariants["border" + color]} ${className}`}>
-        <p className={`text-4xl ${colorVariants["text" + color]}`}>{title}</p>
-        {children}
-      </div>
-    );
-  }
-
-  function InnerCard({ title, link = "", tools = "", date = "", funFact = "", children }:
-      { title: string, link?: string, tools?: string, date?: string, funFact?: string | React.ReactNode, children: React.ReactNode }) {
-    return (
-      <div className="border-2 p-4 border-gray-900 dark:border-gray-100">
-        <span className="text-2xl">
-          {link !== "" ? (<a href={link} target="_blank">{title}</a>) : (title)}
-        </span>
-        {tools !== "" && (
-          <span className="ml-4 text-lg text-gray-500 dark:text-gray-400">{tools}</span>
-        )}
-        {date !== "" && (
-          <span className="float-right text-xl text-gray-600 dark:text-gray-300"><i>{date}</i></span>
-        )}
-        <p className="mb-2" />
-        <p className="text-lg text-gray-800 dark:text-gray-200">
-          {children}
-          {funFact !== "" && (
-            <>
-              <br />
-              <span className="text-xs">{funFact}</span>
-            </>
-          )}
-        </p>
-      </div>
-    );
-  }
-
   return (
     <div className="m-16 text-center font-medium text-black dark:text-white">
       <p className="text-7xl">Franklin Zhu</p>
@@ -128,7 +81,7 @@ export default function Home() {
         >
           <InnerCard
             title={"Infosys"}
-            tools={"Software Engineering Internship"}
+            tools={"Software Engineering Internship in Bangalore, Karnataka, India"}
             date={"Incoming Summer 2026"}
           >
             Excited to be integrating AI with the EdgeVerve team!
