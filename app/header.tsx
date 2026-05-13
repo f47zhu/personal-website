1f368b2488eb7815cd20fb5522c738b5a4266058
f47zhu <f47zhu@uwaterloo.ca>
2026-05-12 22:22:49 -0400

    nits: Clean up header

--- app/header.tsx
+++ app/header.tsx
@@ -1,34 +1,29 @@
+function HeaderElement( {link, title, color}: {link: string, title: string, color: string} ) {
+  const colorVariants: Record<string, string> = {
+    textindigo: "text-indigo-700 dark:text-indigo-300",
+    textgreen: "text-green-700 dark:text-green-300",
+    textred: "text-red-700 dark:text-red-300",
+    textorange: "text-orange-700 dark:text-orange-300",
+    textyellow: "text-yellow-700 dark:text-yellow-300"
+  };
+  return (
+    <a href={link} target={(link[0] !== '#') ? "_blank" : ""}>
+      <div className={`px-[5vw] py-[2vh] text-center hover:bg-gray-400 dark:hover:bg-gray-600 ${colorVariants["text" + color]}`}>
+        {title}
+      </div>
+    </a>
+  );
+}
+
 export function PageHeader() {
   return <>
     <span id={"about"} />
     <header className="z-50 h-15 sticky top-0 border-b-2 grid grid-cols-5 overflow-hidden place-items-center text-xl tracking-[2] font-extrabold bg-white dark:bg-black">
-        <a href="./resume" target="_blank">
-          <div className="px-[5vw] py-[2vh] text-center text-indigo-700 dark:text-indigo-300 hover:bg-gray-400 dark:hover:bg-gray-600">
-            Resume
-          </div>
-        </a>
-        <a href="#about">
-          <div className="px-[5vw] py-[2vh] text-center text-green-700 dark:text-green-300 hover:bg-gray-400 dark:hover:bg-gray-600">
-            About Me
-          </div>
-        </a>
-        <a href="#projects">
-          <div className="px-[5vw] py-[2vh] text-center text-red-700 dark:text-red-300 hover:bg-gray-400 dark:hover:bg-gray-600">
-            Projects
-          </div>
-        </a>
-        <a href="#experience">
-          <div className="px-[5vw] py-[2vh] text-center text-orange-700 dark:text-orange-300 hover:bg-gray-400 dark:hover:bg-gray-600">
-            Experience
-          </div>
-        </a>
-        <a href="#contact">
-          <div className="px-[5vw] py-[2vh] text-centerext-yellow-700 dark:text-yellow-300 hover:bg-gray-400 dark:hover:bg-gray-600">
-            Contact
-          </div>
-        </a>
+        <HeaderElement link="./resume" title="Resume" color="indigo" />
+        <HeaderElement link="#about" title="About Me" color="green" />
+        <HeaderElement link="#projects" title="Projects" color="red" />
+        <HeaderElement link="#experience" title="Experience" color="orange" />
+        <HeaderElement link="#contact" title="Contact" color="yellow" />
     </header>
   </>;
 }
-
-// to-do: outsource header spam to a function
