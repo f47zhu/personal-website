7b8d327d9d38c2e2fbcc59002b378d7fe2bc3c05
f47zhu <f47zhu@uwaterloo.ca>
2026-05-21 22:03:26 -0400

    fix: Prevent popUp from accidentally unpausing carousel

--- app/page.tsx
+++ app/page.tsx
@@ -114,7 +114,6 @@ export default function Home() {
                     </PopUp>
                   ]}
                 />
-                
               </div>
             </div>
           </PageCard>
--- app/popUp.tsx
+++ app/popUp.tsx
@@ -10,8 +10,8 @@ export function PopUp({ children, content, caption = undefined }: { children: Re
     <>
       <div className="hover:cursor-pointer" onClick={() => setIsActive(true)}>{children}</div>
       {(isActive) && createPortal(
-        <div className="z-50 flex flex-col fixed w-screen h-screen place-items-center justify-center gap-4 bg-[#00000080]">
-          <div className="flex flex-col rounded-2xl divide-y-2 divide-gray-400 dark:divide-gray-600 border-2 border-gray-400 dark:border-gray-600">
+        <div className="z-50 flex flex-col fixed w-screen h-screen place-items-center justify-center gap-4 bg-[#00000080]" onClick={() => setIsActive(false)}>
+          <div className="flex flex-col rounded-2xl divide-y-2 divide-gray-400 dark:divide-gray-600 border-2 border-gray-400 dark:border-gray-600" onClick={e => e.stopPropagation()}>
             <>{content}</>
             {(caption !== undefined) && (
               <div className="p-4 rounded-b-2xl text-base text-center bg-white dark:bg-black">
@@ -19,7 +19,7 @@ export function PopUp({ children, content, caption = undefined }: { children: Re
               </div>
             )}
           </div>
-          <button className="py-2 px-4 text-base rounded-full border-2 border-gray-400 dark:border-gray-600 bg-white dark:bg-black" onClick={() => setIsActive(false)}>
+          <button className="py-2 px-4 text-base rounded-full border-2 border-gray-400 dark:border-gray-600 bg-white dark:bg-black">
             Back
           </button>
         </div>, document.body
